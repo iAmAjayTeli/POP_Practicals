03dc6fdce57aa4e81fcc6152475159d22cfbb63a
Ajay Teli <120497280+iAmAjayTeli@users.noreply.github.com>
2024-12-17 19:39:54 +0530

    Create IsArmstrong.c

---
+++ IsArmstrong.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+
+bool isArmstrong (int num){
+    int originalNum=num,power=0, resultNum=0;
+     
+     int temp=num;
+     
+     while(temp!=0){
+         temp/=10;
+         power++;
+     }
+     
+     temp=num;
+     while(temp!=0){
+         int rem=temp%10;
+         resultNum+=pow(rem, power);
+         temp/=10;
+     }
+    
+    return resultNum==num;
+}
+
+int main(){
+    int num=153;
+    
+    if(isArmstrong(num)){
+       printf("%d is Armstrong number", num);
+    }
+    else{
+         printf("%d is not Armstrong number", num);
+    }
+}
